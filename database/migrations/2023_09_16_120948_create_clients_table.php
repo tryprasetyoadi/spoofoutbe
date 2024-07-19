ce1c2d0fbf3726d0719878ce91ea3ce231d7c8fb
prasetyo <tryprasetyoadi2420@gmail.com>
2024-07-19 14:52:50 +0700

    [ADD] : seeding database

---
+++ database/migrations/2023_09_16_120948_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('token');
+            $table->double('liveness_range');
+            $table->double('liveness_threshold');
+            $table->double('fr_range');
+            $table->double('fr_threshold');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
